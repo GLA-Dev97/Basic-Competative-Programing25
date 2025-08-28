ef4098c3ce1fda9fdcd36bd3276015a1bba02ef0
GLA-Dev97 <devesh.raghuvanshi_bac24@gla.ac.in>
2025-08-28 23:16:38 +0530

    Added new Sheet2

---
+++ Week1/Sheet2/06_SumEven.py
@@ -0,0 +1,5 @@
+N = int(input("Enter A: "))
+even_sum = 0
+for i in range(2, N+1, 2):
+    even_sum += i
+print(even_sum)
---
+++ Week1/Sheet2/07_SumOdd.py
@@ -0,0 +1,5 @@
+N = int(input("Enter A: "))
+odd_sum = 0
+for i in range(1, N+1, 2):
+    odd_sum += i
+print(odd_sum)
