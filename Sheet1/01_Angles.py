10ce5fbe0493fdc6d2583d3b37473b923c6be99a
GLA-Dev97 <devesh.raghuvanshi_bca24@gla.ac.in>
2025-08-28 19:38:45 +0530

    Update 01_Angles.py

--- Sheet1/01_Angles.py
+++ Sheet1/01_Angles.py
@@ -1,7 +1,13 @@
-angle = int(input("Enter the Angle:"))
-if(angle == 90):
-    print("Right tringle ")
-elif(angle<=90):
-    print("Acute Tringle")
+a = int(input("Enter the Angle a:"))
+b = int(input("Enter the Angle b:"))
+c = int(input("Enter the Angle c:"))
+
+if(a+b+c ==180):
+    if(a == 90 or b == 90 or c ==90):
+        print("Right tringle ")
+    elif(a > 90 or b >90 or c > 90):
+        print("Obtuse Tringle")
+    else:
+        print("Acute Triangle")
 else:
-    print("Obtuse Triangle")
+    print("Not a valid Triangle")
